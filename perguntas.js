3cc466ffd2527e23d2902fde83195fc2a3ba3087
Vitoria524 <geraldo.vitoria@escola.pr.gov.br>
2025-08-28 15:39:45 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Robótica',
+    'O que significa a sigla USB?',
+    'Universal Serial Bus'
+)
+
+criaCartao(
+    'Química',
+    'Qual a fórmula química da água?',
+    'H2O'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual o maior orgão do corpo humano?',
+    'A pele'
+)
+
+criaCartao(
+    'Artes',
+    'Quem pintou a Monalisa?',
+    'Leonardo Da Vinci'
+)
